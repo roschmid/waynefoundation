8fb29654c8617ce0cac62fab4d77a1032e820712
roschmid <schmidthernandez@gmail.com>
2020-08-02 14:41:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-raw_data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'], 
-        'last_name': ['Miller', 'Jacobson', 'Ali', 'Milner', 'Cooze'], 
-        'age': [42, 52, 36, 24, 73], 
-        'preTestScore': [4, 24, 31, 2, 3],
-        'postTestScore': [25, 94, 57, 62, 70]}
-df = pd.DataFrame(raw_data, columns = ['first_name', 'last_name', 'age', 'preTestScore', 'postTestScore'])
+df = pd.read_excel('Data.xlsx')
 
 #Layout functions
 
